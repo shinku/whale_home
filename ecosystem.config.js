ef775dc4d900bb6ebc3d9f928e84d7abfa1592a0
shinku <jsgxx00@gmail.com>
2025-08-24 20:18:28 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: 'next-app',
       script: 'npm',
       args: 'start',
-      instances: 'max', // 根据CPU核心数启动多个实例
-      exec_mode: 'cluster', // 使用集群模式
+      instances: '1', // 根据CPU核心数启动多个实例
+      exec_mode: 'fork', // 使用集群模式
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
