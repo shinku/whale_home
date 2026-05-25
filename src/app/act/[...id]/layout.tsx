5a6510a8167faf38cd3cdcd92b67fd9590a274f8
shinku <jsgxx00@gmail.com>
2026-05-25 20:26:23 +0800

    feat: change name

--- src/app/act/[...id]/layout.tsx
+++ src/app/act/[...id]/layout.tsx
@@ -1,33 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './style.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./style.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AI写作',
-  description: '移动端活动页面'
-}
+  title: "鲸鱼擦除君",
+  description: "移动端活动页面",
+};
 export const viewport = {
-   width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false
-}
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
-export default function ActLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  
+export default function ActLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN" className={inter.className}>
       <body className="mobile-container">
-        <div className="mobile-viewport">
-          {children}
-        </div>
+        <div className="mobile-viewport">{children}</div>
       </body>
     </html>
-  )
+  );
 }
--- src/app/act/[...id]/page.tsx
+++ src/app/act/[...id]/page.tsx
@@ -1,44 +1,59 @@
-import type { Metadata } from 'next';
-import Script from 'next/script';
+import type { Metadata } from "next";
+import Script from "next/script";
 import AiMathTeacher from "./subpage/client/AiMathTeacher";
 import AIWriter from "./subpage/client/AIWriter";
-import { Common } from './subpage/client/components/Common';
-import InputPage from './subpage/client/InputPage';
+import { Common } from "./subpage/client/components/Common";
+import InputPage from "./subpage/client/InputPage";
 
 type PageProps = {
-  params: Promise<{id:string[]}>
-}
+  params: Promise<{ id: string[] }>;
+};
 
-export async function generateMetadata({ params }: PageProps ): Promise<Metadata> {
-  const actId = (await params).id[0]
+type TActType = "aiwriter" | "math" | "inputer";
+
+const actMapTitle: Record<TActType, string> = {
+  aiwriter: "AI写作",
+  inputer: "上传",
+  math: "智能口算练习",
+};
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const actId = (await params).id[0] as TActType;
   return {
-    title: actId === 'math' ? '智能口算练习' : 'AI写作'
-  }
+    title: actMapTitle[actId],
+  };
 }
 
-const actMap:{[key:string]: React.FC} = {
-  "aiwriter":()=><AIWriter/>,
-  "math":()=><AiMathTeacher/>,
-  'inputer':()=><InputPage/> // 默认页面
-}
+const actMap: Record<TActType, React.FC> = {
+  aiwriter: () => <AIWriter />,
+  math: () => <AiMathTeacher />,
+  inputer: () => <InputPage />, // 默认页面
+};
 
 export default async function Page({ params }: PageProps) {
-  const {id} = await params
-  const actId = id[0]
-  const Comp = actMap[actId] as React.FC
-   return (
+  const { id } = await params;
+  const actId = id[0] as TActType;
+  const Comp = actMap[actId] as React.FC;
+  return (
     <>
-      <Script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js" strategy="beforeInteractive" />
-      <div style={{
-        maxWidth: "768px",
-        margin: "0 auto"
-      }}>
-      {
-        Comp && <Common>
-          <Comp/>
-        </Common>
-      }
+      <Script
+        src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js"
+        strategy="beforeInteractive"
+      />
+      <div
+        style={{
+          maxWidth: "768px",
+          margin: "0 auto",
+        }}
+      >
+        {Comp && (
+          <Common>
+            <Comp />
+          </Common>
+        )}
       </div>
     </>
-  )
+  );
 }
